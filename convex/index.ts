dff4e1f586e57ff35c4bd002add1690ca9623f63
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-11-19 15:40:24 +0700

    feat: retry, just in case

--- convex/index.ts
+++ convex/index.ts
@@ -2,6 +2,8 @@ import { ActionRetrier } from "@convex-dev/action-retrier";
 import { WorkflowManager } from "@convex-dev/workflow";
 import { components } from "./_generated/api";
 
-export const workflow = new WorkflowManager(components.workflow);
+export const workflow = new WorkflowManager(components.workflow, {
+  workpoolOptions: { retryActionsByDefault: true },
+});
 
 export const retrier = new ActionRetrier(components.actionRetrier);
