5976c368a66dfe3a8abed394001807826b4da2aa
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-11-23 19:37:05 +0700

    feat: proper overflow behavior

--- app/(app)/chats/layout.tsx
+++ app/(app)/chats/layout.tsx
@@ -2,8 +2,8 @@ import { ContactsLeftSidebar } from "./_components/contacts-left-sidebar";
 
 export default function ChatLayout({ children }: LayoutProps<"/chats">) {
   return (
-    <main className="flex size-full min-h-(--chat-page-height) flex-row [--chat-page-height:calc(100vh-var(--header-height))]">
-      <section className="w-80 border-r p-1">
+    <main className="flex size-full h-full max-h-(--chat-page-height) flex-row [--chat-page-height:calc(100vh-var(--header-height))]">
+      <section className="w-80 overflow-y-auto border-r p-1">
         <ContactsLeftSidebar />
       </section>
       <section className="flex-1">{children}</section>
