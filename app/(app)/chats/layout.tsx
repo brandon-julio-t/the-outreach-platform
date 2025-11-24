cdc28147d87e217fd7c0b3e22ea93db272fff8b2
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-11-24 14:05:46 +0700

    feat: introduce resizable panel to also fix textarea width from growing when user text is very long

--- app/(app)/chats/layout.tsx
+++ app/(app)/chats/layout.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { ContactsLeftSidebar } from "./_components/contacts-left-sidebar";
 
 export default function ChatLayout({ children }: LayoutProps<"/chats">) {
   return (
-    <div className="flex size-full h-(--page-height) flex-row">
-      <ContactsLeftSidebar />
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="flex size-full h-(--page-height) min-w-0 flex-row"
+    >
+      <ResizablePanel defaultSize={25} className="hidden md:block">
+        <ContactsLeftSidebar />
+      </ResizablePanel>
 
-      <section className="flex-1">{children}</section>
-    </div>
+      <ResizableHandle />
+
+      <ResizablePanel defaultSize={75}>{children}</ResizablePanel>
+    </ResizablePanelGroup>
   );
 }
